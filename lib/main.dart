302f66f8a1ad160e5141d33da2f231de7f959f87
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-15 22:38:04 +0545

    google fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Bell",
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.deepPurple[50],
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.deepPurple[100],
+            elevation: 0,
+            centerTitle: true,
+          )),
       home: HomePage(),
     );
   }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,8 +8,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Class Notification"),
-        centerTitle: true,
+        title: Text(
+          "Class Notification",
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Colors.black,
+          ),
+        ),
       ),
     );
   }
