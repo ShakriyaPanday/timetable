89276934449380e0408b86aa5f8988f116ef3afd
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-17 23:15:46 +0545

    Font changed

--- lib/utlis/currenttime.dart
+++ lib/utlis/currenttime.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CurrentTime extends StatefulWidget {
   const CurrentTime({super.key});
@@ -12,32 +13,63 @@ class CurrentTime extends StatefulWidget {
 class _CurrentTimeState extends State<CurrentTime> {
   int currentHour = (DateTime.now().hour % 12);
   int currentMinute = DateTime.now().minute;
-  String timeRemain = "No Class";
-  String currentSection = "7 D";
+  String timeRemain = "35";
+  String currentSection = "No Period";
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
               child: Text(
-                  currentHour.toString() + " : " + currentMinute.toString()),
+                currentHour.toString() + " : " + currentMinute.toString(),
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                  color: Colors.black,
+                ),
+              ),
             ),
-            Container(child: Text(currentSection)),
+            Container(
+              child: Text(
+                timeRemain,
+                style: GoogleFonts.dancingScript(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 300,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Container(
+                child: Text(
+              currentSection,
+              style: GoogleFonts.ubuntu(
+                fontSize: 50,
+                color: Colors.black,
+              ),
+            )),
             GestureDetector(
               onTap: () {
                 setState(() {
                   currentHour = (DateTime.now().hour % 12);
                   currentMinute = DateTime.now().minute;
-                  currentSection = calculateTime(currentHour, currentMinute);
+                  currentSection = sectionFind(currentHour, currentMinute);
                 });
               },
               child: Container(
-                child: Text("Designed by Shakriya Panday"),
+                child: Text(
+                  "Designed by Shakriya Panday",
+                  style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.black,
+                  ),
+                ),
               ),
             ),
           ],
@@ -47,57 +79,57 @@ class _CurrentTimeState extends State<CurrentTime> {
   }
 }
 
-String calculateTime(int currentHour, int currentMinute) {
+String sectionFind(int currentHour, int currentMinute) {
   //First Period
-  if (currentHour == 10 && currentMinute >= 44 && currentMinute <= 46) {
+  if (currentHour == 10 && currentMinute > 44 && currentMinute <= 46) {
     return "Section : 7 \"D\"";
   }
   //Break
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Break";
   }
   //Second Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"D\"";
   }
   // Third Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"B\"";
   }
   // Lunch
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Lunch Break";
   }
   // Fourth Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"B\"";
   }
   // Fifth Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"C\"";
   }
   //Break
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Break";
   }
   // Sixth Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"C\"";
   }
   // Seventh Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"A\"";
   }
   // Snack
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Snack Break";
   }
   // Eigth Period
-  else if (currentHour == 10 && currentMinute >= 29 && currentMinute <= 32) {
+  else if (currentHour == 10 && currentMinute > 29 && currentMinute <= 32) {
     return "Section : 7 \"A\"";
   }
   // No Period
   else {
-    return "No Period";
+    return "Lunch Break";
   }
 }
