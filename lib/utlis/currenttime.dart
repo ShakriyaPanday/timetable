d1ad173dcf574f75be531586072606f0f9323134
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-23 21:43:26 +0545

    No Class screen updated

--- lib/utlis/currenttime.dart
+++ lib/utlis/currenttime.dart
@@ -10,21 +10,10 @@ class CurrentTime extends StatefulWidget {
 }
 
 class _CurrentTimeState extends State<CurrentTime> {
-  String time = DateFormat.jm().format(DateTime.now()).toString();
+  String time = DateFormat.E().format(DateTime.now()).toString();
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Period(),
-            ],
-          ),
-        ),
-      ),
-    );
+    return time == 'We' ? const Period() : const NoClass();
   }
 }
 
@@ -234,86 +223,91 @@ class _PeriodState extends State<Period> {
   String time = DateFormat.jm().format(DateTime.now()).toString();
   int currentHour = (DateTime.now().hour);
   int currentMinute = DateTime.now().minute;
-  String timeRemain = "30";
+  String timeRemain = "";
   String currentSection = "No Period";
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          time,
-          style: GoogleFonts.oswald(
-            fontWeight: FontWeight.bold,
-            fontSize: 70,
-            color: Colors.black,
-          ),
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        Container(
-          width: 600,
-          decoration: BoxDecoration(
-            color: Colors.deepOrange[300],
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Center(
-            child: Text(
-              timeRemain,
-              style: GoogleFonts.ubuntu(
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              time,
+              style: GoogleFonts.oswald(
                 fontWeight: FontWeight.bold,
-                fontSize: 300,
-                color: Colors.white,
+                fontSize: 70,
+                color: Colors.black,
               ),
             ),
-          ),
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        Text(
-          currentSection,
-          style: GoogleFonts.oswald(
-            fontSize: 50,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            wordSpacing: 5,
-          ),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              time = DateFormat.jm().format(DateTime.now()).toString();
-              currentHour = (DateTime.now().hour);
-              currentMinute = DateTime.now().minute;
-              timeRemain = timeRemains(currentHour, currentMinute);
-              currentSection = sectionFind(currentHour, currentMinute);
-            });
-          },
-          child: SizedBox(
-            width: double.infinity,
-            child: Center(
-              child: Text(
-                "Designed by Shakriya Panday",
-                style: GoogleFonts.oswald(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.black,
+            const SizedBox(
+              height: 60,
+            ),
+            Container(
+              width: 600,
+              decoration: BoxDecoration(
+                color: Colors.deepOrange[300],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  timeRemain,
+                  style: GoogleFonts.ubuntu(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 300,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
+            const SizedBox(
+              height: 60,
+            ),
+            Text(
+              currentSection,
+              style: GoogleFonts.oswald(
+                fontSize: 50,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                wordSpacing: 5,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  time = DateFormat.jm().format(DateTime.now()).toString();
+                  currentHour = (DateTime.now().hour);
+                  currentMinute = DateTime.now().minute;
+                  timeRemain = timeRemains(currentHour, currentMinute);
+                  currentSection = sectionFind(currentHour, currentMinute);
+                });
+              },
+              child: SizedBox(
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    "Designed by Shakriya Panday",
+                    style: GoogleFonts.oswald(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
@@ -323,6 +317,23 @@ class NoClass extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.deepOrange[300]),
+          child: Center(
+            child: Text(
+              "No Class",
+              style: GoogleFonts.oswald(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
