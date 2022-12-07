5a007b5acd3eee10f4346471eba6cc03e99f8487
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-12-07 22:13:27 +0545

    container decorated

--- lib/utlis/periodcheck.dart
+++ lib/utlis/periodcheck.dart
@@ -21,17 +21,61 @@ class _PeriodCheckState extends State<PeriodCheck> {
               Navigator.push(context,
                   MaterialPageRoute(builder: ((context) => const Period())));
             }),
-            child: const Text("Normal"),
+            child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30.0),
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange[300],
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 4),
+                        blurRadius: 20.0)
+                  ],
+                ),
+                child: Center(
+                    child: Text(
+                  "Normal Hour",
+                  style: TextStyle(
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange[50],
+                  ),
+                ))),
           ),
-          SizedBox(
-            height: 100,
+          const SizedBox(
+            height: 50,
           ),
           GestureDetector(
             onTap: (() {
               Navigator.push(context,
                   MaterialPageRoute(builder: ((context) => const ZeroHour())));
             }),
-            child: const Text("Zero"),
+            child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30.0),
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange[300],
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 4),
+                        blurRadius: 20.0)
+                  ],
+                ),
+                child: Center(
+                    child: Text(
+                  "Zero Hour",
+                  style: TextStyle(
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange[50],
+                  ),
+                ))),
           )
         ],
       ),
